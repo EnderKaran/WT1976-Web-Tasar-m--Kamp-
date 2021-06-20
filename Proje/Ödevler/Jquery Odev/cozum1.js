a88391b0b8038d0eaeb2b36c1162394a3684dad2
EnderKaran <76999313+EnderKaran@users.noreply.github.com>
2021-06-20 23:01:56 +0300

    Web Tasarım Kampı
    Ödev Ve Proje

---
+++ Proje/Ödevler/Jquery Odev/cozum1.js
@@ -0,0 +1,31 @@
+// görev 1
+$("#listele").click(function(){
+    $("#gorev1>ul").prepend("<li>1. Babies have around 100 more bones than adults</li>");
+    $("#gorev1>ul").append("<li>2. The Eiffel Tower can be 15 cm taller during the summer</li>");
+    $("#gorev1>ul").append("<li>3. 20% of Earth’s oxygen is produced by the Amazon rainforest</li>");
+});
+// görev 2
+
+// görev 3
+$('#gonder').click(function () {
+    $('#text').val("15 amazing science facts that will blow your mind");
+});
+// görev 4
+
+// görev 5
+$(document).ready(function(){
+    $("#renk").click(function(){
+        $("#h1").css({"color":"red"});
+        $("div>#tek").css({"color":"blue"});
+        $("div>#cift").css({color:"orange"});
+    });
+});
+// görev 6
+
+// görev 7
+
+// görev 8
+
+// görev 9
+
+// görev 10
